06075c2896f14fd6329c961d82b710850e38414e
hymanHu <hujiangyx@163.com>
2019-10-14 17:11:00 +0800

    hrms

---
+++ hrms/src/main/java/com/hqyj/hrms/controller/AccountController.java
@@ -0,0 +1,19 @@
+package com.hqyj.hrms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hrms")
+public class AccountController {
+
+	@RequestMapping("/login")
+	public String login() {
+		return "/login";
+	}
+	
+	@RequestMapping("/register")
+	public String register() {
+		return "/register";
+	}
+}
---
+++ hrms/src/main/java/com/hqyj/hrms/entity/Department.java
@@ -0,0 +1,24 @@
+package com.hqyj.hrms.entity;
+
+public class Department {
+
+	private int departmentId;
+	private String departmentName;
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+
+}
