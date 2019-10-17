aaf51a57ad7f8fa3e4c68d57ccd4121fc19549e8
hymanHu <hujiangyx@163.com>
2019-10-17 16:10:56 +0800

    test

---
+++ spring_boot_shiro/src/main/java/com/example/demo/SpringBootShiroApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShiroApplication.class, args);
+		
+//		SpringApplication application = new SpringApplication(SpringBootShiroApplication.class);
+//		application.setBannerMode(Banner.Mode.OFF);
+//		application.run(args);
+	}
+
+}
