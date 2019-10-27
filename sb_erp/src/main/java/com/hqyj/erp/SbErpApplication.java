a3f478cb5d88bd2b1a80055b6ee58d9430d2b922
hymanHu <hujiangyx@163.com>
2019-10-27 10:18:50 +0800

    "test"

---
+++ sb_erp/src/main/java/com/hqyj/erp/SbErpApplication.java
@@ -0,0 +1,13 @@
+package com.hqyj.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbErpApplication.class, args);
+	}
+
+}
---
+++ sb_erp/src/main/java/com/hqyj/erp/config/MyRealm.java
@@ -0,0 +1,57 @@
+package com.hqyj.erp.config;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRealm extends AuthorizingRealm {
+	
+//	@Autowired
+//	private AccountService accountService;
+
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		// 授权类
+		SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+		
+//		// 获取user信息
+//		String userName = (String) principals.getPrimaryPrincipal();
+//		User user = accountService.getUserByName(userName);
+//		if (user == null) {
+//			return null;
+//		}
+//		
+//		// 获取角色信息，配合页面标签，确定不同角色访问不同的资源
+//		List<Role> roles = accountService.getRolesByUserId(user.getUserId());
+//		for (Role role : roles) {
+//			authorizationInfo.addRole(role.getRoleName());
+//			List<Resource> resources = accountService.getResourcesByRoleId(role.getRoleId());
+//			for (Resource resource : resources) {
+//				authorizationInfo.addStringPermission(resource.getPermission());
+//			}
+//		}
+		
+		return authorizationInfo;
+	}
+
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+//		String userName = (String) token.getPrincipal();
+//		User user = accountService.getUserByName(userName);
+//		if (user == null) {
+//			throw new UnknownAccountException("The account do not exist.");
+//		}
+		
+		// realmName: 当前 realm 对象的唯一名字. 调用父类的 getName() 方法
+//		return new SimpleAuthenticationInfo(userName, user.getPassword(), getName());
+		return new SimpleAuthenticationInfo("admin", "admin", getName());
+	}
+
+}
