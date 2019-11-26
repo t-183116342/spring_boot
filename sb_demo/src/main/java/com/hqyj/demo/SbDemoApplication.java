cc8c9c3bd26384712e98af045718a8cbdb305943
hymanHu <hujiangyx@163.com>
2019-11-26 14:59:42 +0800

    "spring boot test."

--- sb_demo/src/main/java/com/hqyj/demo/SbDemoApplication.java
+++ sb_demo/src/main/java/com/hqyj/demo/SbDemoApplication.java
@@ -2,12 +2,15 @@ package com.hqyj.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.Banner;;
 
 @SpringBootApplication
 public class SbDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbDemoApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(SbDemoApplication.class);
+//		springApplication.setBannerMode(Banner.Mode.OFF);
+		springApplication.run();
 	}
 
 }
